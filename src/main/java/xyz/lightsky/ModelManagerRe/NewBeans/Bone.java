3355c8d1d1c531a92d3c49857bb865dd5368151e
iGxnon <1545766400@qq.com>
2021-07-11 08:23:22 +0800

    bone rotation added

--- src/main/java/xyz/lightsky/ModelManagerRe/NewBeans/Bone.java
+++ src/main/java/xyz/lightsky/ModelManagerRe/NewBeans/Bone.java
@@ -6,11 +6,13 @@ public class Bone  {
 
     public String name;
     public List<Double> pivot;
+    public List<Double> rotation;
     public List<Cube> cubes;
 
-    public Bone(String name, List<Double> pivot, List<Cube> cubes) {
+    public Bone(String name, List<Double> pivot, List<Double> rotation, List<Cube> cubes) {
         this.name = name;
         this.pivot = pivot;
+        this.rotation = rotation;
         this.cubes = cubes;
     }
 
